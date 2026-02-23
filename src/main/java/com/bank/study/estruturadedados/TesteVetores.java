0c5ac9e7f90848ec246ea440da3c8781ca5bc99d
Antonio <rodrigowserra@gmail.com>
2026-02-23 13:59:19 +0000

    Bubble Sort

---
+++ src/main/java/com/bank/study/estruturadedados/TesteVetores.java
@@ -0,0 +1,20 @@
+package com.bank.study.estruturadedados;
+
+public class TesteVetores {
+
+	public static void main(String[] args) {
+		int[] vetor = {5,9,10,20,7};
+		int j = 0;
+		System.out.println("vetor[j]: " + vetor[j]);
+		System.out.println("j: " + j);
+		System.out.println("j+1: " + j+1);
+		
+		System.out.println("vetor[1]: " + vetor[1]);
+		System.out.println("vetor[j+1]: " + vetor[j+1]);
+		System.out.println("j+3: " + j+3);
+		System.out.println("vetor[j+3]: " + vetor[j+3]);
+		
+
+	}
+
+}
